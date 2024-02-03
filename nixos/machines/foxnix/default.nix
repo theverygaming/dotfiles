51bc1bb7dc09615183833a7fd8448ab70f27c3a0
theverygaming <theverygaming@users.noreply.github.com>
2024-02-03 02:12:04 +0100

    nixos: fix renamed options

--- nixos/machines/foxnix/default.nix
+++ nixos/machines/foxnix/default.nix
@@ -28,9 +28,9 @@
   networking.networkmanager.enable = true;
 
   # Configure keymap in X11
-  services.xserver = {
+  services.xserver.xkb = {
     layout = "de";
-    xkbVariant = "";
+    variant = "";
   };
 
   # Enable the X11 windowing system.
