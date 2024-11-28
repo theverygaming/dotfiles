ae0e1ae5dc6d14b5a43479832d1ba7c33be344c7
theverygaming <theverygaming@users.noreply.github.com>
2024-11-28 11:33:53 +0100

    laptop battery saver stuff

--- nixos/machines/foxportable/default.nix
+++ nixos/machines/foxportable/default.nix
@@ -62,4 +62,27 @@
   environment.shells = with pkgs; [ zsh ];
 
   hardware.rtl-sdr.enable = true;
+
+  # this is a thinkpad meow!!
+  # https://www.reddit.com/r/NixOS/comments/1d1v6ev/comment/l603nyi
+  powerManagement.powertop.enable = true;                           # enable powertop auto tuning on startup.
+
+  services.system76-scheduler.settings.cfsProfiles.enable = true;   # Better scheduling for CPU cycles - thanks System76!!!
+  services.thermald.enable = true;                                  # Enable thermald, the temperature management daemon. (only necessary if on Intel CPUs)
+  services.power-profiles-daemon.enable = false;                    # Disable GNOMEs power management
+  services.tlp = {                                                  # Enable TLP (better than gnomes internal power manager)
+    enable = true;
+    settings = { # sudo tlp-stat or tlp-stat -s or sudo tlp-stat -p
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+      CPU_HWP_DYN_BOOST_ON_AC = 1;
+      CPU_HWP_DYN_BOOST_ON_BAT = 0;
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+      START_CHARGE_THRESH_BAT0 = 70;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+    };
+  };
 }
