d775ce6229c3697c3299144e111f54101c7dd11f
theverygaming <theverygaming@users.noreply.github.com>
2024-12-28 17:02:57 +0100

    i3-like keybinds for gnome (for laptop)

--- nixos/users/user/gui/gnome.nix
+++ nixos/users/user/gui/gnome.nix
@@ -106,6 +106,18 @@
       search = [ "<Super>d" ];
     };
 
+    # keybinds: i3-like windowing stuff
+    "org/gnome/desktop/wm/keybindings" = {
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      move-to-workspace-1 = [ "<Super><Shift>1" ];
+      move-to-workspace-2 = [ "<Super><Shift>2" ];
+      move-to-workspace-3 = [ "<Super><Shift>3" ];
+      move-to-workspace-4 = [ "<Super><Shift>4" ];
+    };
+
     # custom keybinds
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       name = "open terminal";
