a86040624b54a3ea801211f6fabd8f7fd25a0851
theverygaming <theverygaming@users.noreply.github.com>
2025-03-18 12:50:44 +0100

    home-manager stuff

--- nixos/users/user/home.nix
+++ nixos/users/user/home.nix
@@ -28,4 +28,11 @@ in
 
   programs.home-manager.enable = true;
   home.stateVersion = "23.05";
+
+  # TODO: this doesn't work i think lol
+  # nix =  {
+  #   channels = {
+  #     nixpkgs = flakeInputs.nixpkgs;
+  #   };
+  # };
 }
