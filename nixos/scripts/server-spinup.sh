4327fd5df67d60132bbd33eab18e9948ea83f4f8
theverygaming <theverygaming@users.noreply.github.com>
2025-03-27 22:13:15 +0100

    server-spinup notes

--- nixos/scripts/server-spinup.sh
+++ nixos/scripts/server-spinup.sh
@@ -11,3 +11,7 @@ nix run github:nix-community/nixos-anywhere \
     --flake "${SCRIPT_DIR}/..#${HOST}" \
     --generate-hardware-config nixos-generate-config "./hosts/${HOST}/hardware-configuration.nix" \
     --target-host "${TARGET}"
+
+# --disk-encryption-keys <remote_path> <local_path>
+#  copy the contents of the file or pipe in local_path to remote_path in the installer environment,
+#  after kexec but before installation. Can be repeated.
