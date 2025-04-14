aa3f04b2414b02934400372f947fccdba2ac5c5a
theverygaming <theverygaming@users.noreply.github.com>
2025-04-14 21:13:26 +0200

    whole bunch of nonsense

--- nixos/configs/wireguard_mesh.nix
+++ nixos/configs/wireguard_mesh.nix
@@ -8,17 +8,17 @@
 
   custom.wg_mesh = {
     enable = true;
-    interface = "wg0";
+    getInterface = n: "wgm${builtins.toString n}";
     privateKeyFile = config.sops.secrets.wireguard_private.path;
     config = {
-      addrv4NetworkAddress = "10.13.12.0";
-      addrv4PrefixLength = 24;
+      addrv4NetworkAddress = "10.69.0.0";
+      addrv4PrefixLength = 16;
       hosts = {
         "vps-1" = {
           publicKey = "3L9OTQ9q534ouK1pn1g2xq9foAOiFU9NDOS3kMT1siE=";
           port = 65436;
           int = {
-            addrv4 = "10.13.12.1";
+            addrv4prefix = "10.69.1";
           };
           networks = {
             "internet" = {
@@ -36,7 +36,7 @@
           publicKey = "OkFYdhclX4kykKHV1AZ+Nlh+mHfiNmhyQ/3kd1Su1yY=";
           port = 65436;
           int = {
-            addrv4 = "10.13.12.2";
+            addrv4prefix = "10.69.2";
           };
           networks = {
             "foxden" = {
@@ -54,7 +54,7 @@
           publicKey = "61OX9FRj85BXMM2nl/teQd0IhURVjmxuXriqp8VEKGw=";
           port = 65436;
           int = {
-            addrv4 = "10.13.12.3";
+            addrv4prefix = "10.69.3";
           };
           networks = {};
           reachableNetworks = [ "internet" "foxden" ];
--- nixos/modules/nixos/wg_mesh.nix
+++ nixos/modules/nixos/wg_mesh.nix
@@ -16,7 +16,7 @@ in
         options = {
           addrv4NetworkAddress = mkOption {
             type = types.str;
-            description = "IPv4 petwork address for the whole WireGuard network";
+            description = "IPv4 network address for the whole WireGuard network";
           };
           addrv4PrefixLength = mkOption {
             type = types.int;
@@ -41,9 +41,9 @@ in
                     };
 
                     int = {
-                      addrv4 = mkOption {
+                      addrv4prefix = mkOption {
                         type = types.str;
-                        description = "Internal WireGuard IPv4 address";
+                        description = "Internal WireGuard IPv4 address prefix";
                       };
                     };
 
@@ -91,10 +91,10 @@ in
       '';
     };
 
-    interface = mkOption {
-      type = types.str;
+    getInterface = mkOption {
+      type = types.anything;
       description = ''
-        WireGuard interface to use (wgX)
+        A function that, based on a number input (starting from zero), returns a WireGuard interface to use (wgX) as string
       '';
     };
 
@@ -161,44 +161,166 @@ in
       currentHost = cfg.config.hosts."${currentHostName}";
     in
     {
-      /*
-        boot.kernel.sysctl = {
-          "net.ipv4.conf.all.forwarding" = true;
-          "net.ipv6.conf.all.forwarding" = true;
-        };
-      */
+      # big thanks to https://www.kepstin.ca/blog/babel-routing-over-wireguard-for-the-tubes/ :3
+
+      ## WireGuard
 
       networking.firewall = {
         allowedUDPPorts = [ currentHost.port ];
       };
 
       networking.wireguard.enable = true;
-      networking.wireguard.interfaces."${cfg.interface}" = {
-        ips = [ "${currentHost.int.addrv4}/${builtins.toString cfg.config.addrv4PrefixLength}" ];
-        listenPort = currentHost.port;
-
-        privateKeyFile = cfg.privateKeyFile;
-
-        peers =
-          let
-            peerDefaults = peerName: peer: {
-              name = peerName;
-              publicKey = peer.publicKey;
-              allowedIPs = [
-                "${cfg.config.addrv4NetworkAddress}/${builtins.toString cfg.config.addrv4PrefixLength}"
-              ];
-              persistentKeepalive = 25;
-            };
-          in
-          (lib.attrsets.mapAttrsToList (
-            peerName: peer:
+      networking.wireguard.interfaces = builtins.listToAttrs (
+        let
+          interfaceGen =
             {
-              endpoint = peerEndpoint currentHostName peerName;
-            }
-            // (peerDefaults peerName peer)
-          ) (reachableHostsFromHost currentHostName))
-          ++ (lib.attrsets.mapAttrsToList peerDefaults (hostsThatCanReachHost currentHostName));
-      };
+              peerName,
+              peer,
+              hasEndpoint,
+              interfaceIdx,
+            }:
+            {
+              ips = [
+                "${currentHost.int.addrv4prefix}.${builtins.toString (interfaceIdx+1)}/32"
+                # Babel link-local address
+                "fe80:${
+                  # Babel needs a link-local IPv6 address that is unique at least on each link.
+                  # We generate an address based on this machine's name, which should be unique!
+                  # I'm sorry. I started writing this at 05:30am and finished at 06:33am
+                  # Also, this breaks if at least two machine's honstnames start with the same N (see below) characters
+                  # but if that is the case i think something else is seriously wrong
+                  let
+                    strToBytes = str: builtins.map lib.strings.charToInt (lib.stringToCharacters str);
+                    asciiByteToHex = ascii: (if ascii < 16 then "0" else "") + (lib.toHexString ascii);
+                    byteArrToHexString = arr: builtins.foldl' (s: el: s + (asciiByteToHex el)) "" arr;
+                    padString =
+                      str: max: pad:
+                      if builtins.stringLength str > max then
+                        builtins.substring 0 max str
+                      else
+                        lib.concatStrings ([ str ] ++ (builtins.genList (_: pad) (max - (builtins.stringLength str))));
+                    splitIntoChunks =
+                      list: chunkSize:
+                      builtins.foldl' (
+                        acc: idx:
+                        acc
+                        ++ [
+                          (lib.take chunkSize (lib.drop (idx * chunkSize) list))
+                        ]
+                      ) [ ] (lib.range 0 (((builtins.length list) / chunkSize) - 1));
+                  in
+                  if
+                    lib.asserts.assertMsg
+                      (
+                        let
+                          hosts = builtins.map (x: padString x (14 - 1) "e") (builtins.attrNames cfg.config.hosts);
+                        in
+                        !(builtins.any (s: (builtins.length (builtins.filter (x: x == s) hosts)) > 1) hosts)
+                      )
+                      "two hostnames start with the same 14-1 characters. This breaks the cursed wireguard link-local address assignment thingy"
+                  then
+                    lib.concatStringsSep ":" (
+                      builtins.map byteArrToHexString (
+                        splitIntoChunks (
+                          [
+                            (
+                              if
+                                (lib.asserts.assertMsg (
+                                  interfaceIdx < 256
+                                ) "link-local address assignment skill issue (interfaceIdx too high)!")
+                              then
+                                interfaceIdx
+                              else
+                                0
+                            )
+                          ]
+                          ++ (strToBytes (padString currentHostName (14 - 1) "e"))
+                        ) 2
+                      )
+                    )
+                  else
+                    "UNREACHABLE"
+                }/16"
+              ];
+              listenPort = currentHost.port + interfaceIdx;
+
+              privateKeyFile = cfg.privateKeyFile;
+
+              allowedIPsAsRoutes = false; # Babel's job!
+
+              peers = [
+                (
+                  {
+                    name = peerName;
+                    publicKey = peer.publicKey;
+                    allowedIPs = [
+                      "${cfg.config.addrv4NetworkAddress}/${builtins.toString cfg.config.addrv4PrefixLength}"
+                      # Babel uses IPv6 link-local, unicast and multicast addresses
+                      "fe80::/64"
+                      "ff02::1:6/128"
+                    ];
+                    persistentKeepalive = 25;
+                  }
+                  // (
+                    if hasEndpoint then
+                      {
+                        endpoint = peerEndpoint currentHostName peerName;
+                      }
+                    else
+                      { }
+                  )
+                )
+              ];
+
+            };
+          listReachableHostsFromHost = lib.attrsets.attrsToList (reachableHostsFromHost currentHostName);
+          listReachableHostsFromHostLen = builtins.length listReachableHostsFromHost;
+          listHostsCanReach = lib.attrsets.attrsToList (hostsThatCanReachHost currentHostName);
+          listHostsCanReachLen = builtins.length listHostsCanReach;
+        in
+        (
+          builtins.map (i: {
+            name = cfg.getInterface i;
+            value = interfaceGen (
+              let
+                el = builtins.elemAt listReachableHostsFromHost i;
+              in
+              {
+                peerName = el.name;
+                peer = el.value;
+                hasEndpoint = true;
+                interfaceIdx = i;
+              }
+            );
+          }) (lib.range 0 (listReachableHostsFromHostLen - 1))
+          ++ builtins.map (
+            i:
+            (
+              let
+                el = builtins.elemAt listHostsCanReach i;
+              in
+              {
+                name = cfg.getInterface (i + listReachableHostsFromHostLen);
+                value = interfaceGen {
+                  peerName = el.name;
+                  peer = el.value;
+                  hasEndpoint = false;
+                  interfaceIdx = i + listReachableHostsFromHostLen;
+                };
+              }
+            )
+          ) (lib.range 0 (listHostsCanReachLen - 1))
+        )
+      );
+
+      ## Routing
+
+      /*
+        boot.kernel.sysctl = {
+          "net.ipv4.conf.all.forwarding" = true;
+          "net.ipv6.conf.all.forwarding" = true;
+        };
+      */
     }
   );
 }
