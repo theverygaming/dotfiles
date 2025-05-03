46146fac0dafa102072f7b93f7adb63e39b3af8d
theverygaming <theverygaming@users.noreply.github.com>
2025-05-03 05:36:45 +0200

    add nixpkgs-stable input

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
 
     disko = {
       url = "github:nix-community/disko";
