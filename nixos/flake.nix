650d5a074a2c3b9ba1a80ca857a328f29fedaa24
theverygaming <theverygaming@users.noreply.github.com>
2025-10-12 02:15:25 +0200

    flake update

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     disko = {
       url = "github:nix-community/disko";
