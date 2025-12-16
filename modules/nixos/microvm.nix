bdfa844b96105810c0a36abab63c1c98ee25214e
theverygaming <theverygaming@users.noreply.github.com>
2025-12-16 13:58:32 +0100

    fix microvm option names

--- modules/nixos/microvm.nix
+++ modules/nixos/microvm.nix
@@ -11,7 +11,7 @@ let
 in
 {
   options.custom.microvm = with lib; {
-    enable = mkEnableOption "Enables the WireGuard mesh";
+    enable = mkEnableOption "Enables microvm";
     hostIp = mkOption {
       type = types.str;
       default = "10.69.0.1";
